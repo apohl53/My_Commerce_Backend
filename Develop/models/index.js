52b98218eaa03ef51d336d88eb8ae1957ac672c1
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-25 21:05:16 -0400

    installed mysql2, sequelize, dotenv

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,7 +16,7 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
